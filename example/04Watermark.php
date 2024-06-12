a4a5ff8e28fe62bcea5ea28f41bb1f4c2c02dc24
naomai <lulu@nieznani-sprawcy.pl>
2024-06-12 21:02:51 +0200

    change layering demo example to Thug Life Einstein

--- example/04Watermark.php
+++ example/04Watermark.php
@@ -5,19 +5,30 @@ if(!isset($gdwExample)) {header("Location: Example.php"); exit;}
 echo "<h3>4. Watermark</h1>\n";
 
 // import image as background
-$layersImg = GDW\Image::createFromFile(__DIR__ . "/stroller.jpg");
+$layersImg = GDW\Image::createFromFile(__DIR__ . "/eins.jpg");
 
-// add watermark layer
-$watermarkLayer = GDW\Layer::createFromFile(__DIR__ . "/lg_watermark.png");
-// naming is optional
-$watermarkLayer->name = "Watermark layer";
-// let's put the watermark layer on top of the image
+// create watermark layer
+$watermarkLayer = GDW\Layer::createFromFile(__DIR__ . "/cheesymemz.png");
+
+// attach the layer to image, so we can move things around
 $layersImg->addLayerTop($watermarkLayer);
-
-// we select (like CTRL+A), then drag the contents, and finally apply the result.
+// we're going to move the watermark to bottom left corner
+// make selection (like CTRL+A), then drag the contents, and finally apply the result.
 $watermarkLayer->select()->move(0, GDW\IMAGE_BOTTOM)->apply();
 
+// make things more THUG
+$thugLayer = GDW\Layer::createFromFile(__DIR__ . "/thug.png");
+$layersImg->addLayerTop($thugLayer);
+// moving to a fixed position
+$thugLayer->select()->move(290, 95)->apply();
+
 // export the image as data URL
+$dataUrl = $layersImg->getDataUrlPNG();
+echo "<img src=\"".htmlspecialchars($dataUrl)."\"/><br/>";
+
+// tiled view of layers
+echo "Separate view of different layers:<br/>";
+$layersImg->setComposer(new GDW\Composers\TiledComposer());
 $dataUrl = $layersImg->getDataUrlPNG();
 echo "<img src=\"".htmlspecialchars($dataUrl)."\"/><br/>";
 
