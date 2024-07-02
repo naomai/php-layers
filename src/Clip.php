8929ef7c075beec8c5d93a46edc4c4010c784f00
naomai <lulu@nieznani-sprawcy.pl>
2024-07-02 12:31:22 +0200

    refactor/ clip, tabs, braces

--- src/Clip.php
+++ src/Clip.php
@@ -4,15 +4,15 @@ namespace Naomai\PHPLayers;
 
 class Clip {
     protected $image;
-    public function __construct($gdImage){
-        $img = imagecreatetruecolor(imagesx($gdImage),imagesy($gdImage));
-        imagecopy($img,$gdImage,0,0,0,0,imagesx($gdImage),imagesy($gdImage));
+    public function __construct($gdImage) {
+        $img = imagecreatetruecolor(imagesx($gdImage), imagesy($gdImage));
+        imagecopy($img, $gdImage, 0, 0, 0, 0, imagesx($gdImage), imagesy($gdImage));
         $this->image = $img;
     }
-    public function __destruct(){
+    public function __destruct() {
         imagedestroy($this->image);	
     }
-    public function getContents(){
+    public function getContents() {
         return $this->image;		
     }
 }
