7ca2a01abd1e42b9447a5e6dc6e313c5b8d2aca4
naomai <lulu@nieznani-sprawcy.pl>
2024-07-17 10:43:39 +0200

    test: more coverage for image

--- tests/Unit/ImageTest.php
+++ tests/Unit/ImageTest.php
@@ -30,6 +30,50 @@ final class ImageTest extends TestCase {
         $this->assertInstanceOf(Image::class, $imageObj);
     }
 
+    public function testGetSize() : void {
+        $testImg = static::createEmptyImage();
+        $size = $testImg->getSize();
 
+        $this->assertEquals(['w'=>200, 'h'=>100], $size);
+    }
+
+    public function testSetSize() : void {
+        $testImg = static::createEmptyImage();
+        $testImg->setSize(75, 48);
+        $size = $testImg->getSize();
+        $this->assertEquals(['w'=>75, 'h'=>48], $size);
+    }
+
+    public function testGetLayerByIndex() : void {
+        $testImg = static::createTwoLayerImage();
+        $bg = $testImg->getLayerByIndex(0);
+        $fg = $testImg->getLayerByIndex(1);
+
+        $this->assertNotNull($bg);
+        $this->assertNotNull($fg);
+        $this->assertNotSame($bg, $fg);
+
+        // negative index
+        $last = $testImg->getLayerByIndex(-1);
+        $this->assertSame($fg, $last);
+
+        $underflow = $testImg->getLayerByIndex(-3);
+        $this->assertNull($underflow);
+        $overflow = $testImg->getLayerByIndex(2);
+        $this->assertNull($overflow);
+    }
+
+
+    private static function createEmptyImage() : Image {
+        $testImg = new Image(200, 100);
+        return $testImg;
+    }
+
+    private static function createTwoLayerImage() : Image {
+        $testImg = new Image(200, 100, false);
+        $testImg->newLayer("banana");
+        $testImg->newLayer("strawberry");
+        return $testImg;
+    }
 
 }
