a261a4a5a08f232e14a111642560872931ad637a
naomai <lulu@nieznani-sprawcy.pl>
2024-08-04 14:59:24 +0200

    test: importing into layer

--- tests/Unit/LayerTest.php
+++ tests/Unit/LayerTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Naomai\PHPLayers;
 use Naomai\PHPLayers\Image;
 use Naomai\PHPLayers\Layer;
+use Naomai\PHPLayers\Test\Assert as ImageAssert;
 use PHPUnit\Framework\MockObject\MockObject;
 
 require_once 'src/Image.php';
@@ -14,6 +15,50 @@ require_once 'src/Image.php';
  * @covers Naomai\PHPLayers\Layer
  */
 final class LayerTest extends TestCase {
+
+    public function testImportFromGD() : void {
+        $gdSource = imagecreatetruecolor(50, 60);
+
+        $layerObj = $this->createEmptyLayer();
+        $layerObj->importFromGD($gdSource);
+
+        $dimensions = $layerObj->getSurfaceDimensions();
+
+        $this->assertEquals(
+            ['x'=>0,'y'=>0,'w'=>50,'h'=>60],
+            $dimensions
+        );
+
+    }
+
+    public function testImportFromFile() : void {
+
+        $layerObj = $this->createEmptyLayer();
+        $layerObj->importFromFile(__DIR__ . "/../TestImages/BasicSquares.png");
+
+        $dimensions = $layerObj->getSurfaceDimensions();
+
+        $this->assertEquals(
+            ['x'=>0,'y'=>0,'w'=>50,'h'=>55],
+            $dimensions
+        );
+
+        ImageAssert::assertCallableThrows(
+            \RuntimeException::class, 
+            function() {
+                $layerObj = $this->createEmptyLayer();
+                $layerObj->importFromFile(__DIR__ . "/../TestImages/NonExistingImage.png");
+            }
+        );
+
+        ImageAssert::assertCallableThrows(
+            \RuntimeException::class, 
+            function() {
+                $layerObj = $this->createEmptyLayer();
+                $layerObj->importFromFile(__DIR__ . "/../TestImages/GarbageBytes.dat");
+            }
+        );
+    }
 
     public function testReorder() {
         $testLayer = $this->createAttachedLayer();
@@ -111,18 +156,6 @@ final class LayerTest extends TestCase {
         return $testLayer;
     }
 
-    private function assertCallableThrows(string $className, callable $function){
-        $exceptionClass="";
-        try {
-            call_user_func($function);
-        } catch(\Exception $e){
-            $exceptionClass = get_class($e);
-        }
-        static::assertEquals(
-            $exceptionClass, $className, 
-            "Failed asserting that call throws {$className}"
-        );
 
-    }
 
 }
